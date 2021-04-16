bce926bd92a406a63676414a044e53b2a92089f9
Macheya Steele <mcheysteele@gmail.com>
2021-04-16 16:09:29 -0600

    implementing stripe for payments

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,3 +1,4 @@
+import { useLazyQuery } from '@apollo/react-hooks';
 import React, { useEffect } from "react";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
@@ -7,9 +8,23 @@ import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import "./style.css";
 
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
 
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+  
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
@@ -31,6 +46,21 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+
+    getCheckout({
+      variables: { products: productIds }
+    });
+
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
   }
 
   if (!state.cartOpen) {
@@ -58,9 +88,9 @@ const Cart = () => {
 
             {
               Auth.loggedIn() ?
-                <button>
-                  Checkout
-              </button>
+              <button onClick={submitCheckout}>
+              Checkout
+            </button>
                 :
                 <span>(log in to check out)</span>
             }
